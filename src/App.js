9bf1672968438ad2b457b38e764301bbe0f7a88b
CodingBae <e.brajan.98@gmail.com>
2019-08-13 11:16:01 +0200

    Initial Commit / Fetched Data from API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Grid } from "@material-ui/core";
+import youtube from "./api/youtube";
+import SearchBar from "./components/SearchBar";
+import VideoDetail from "./components/VideoDetail";
+class App extends Component {
+  handleSubmit = async SearchTerm => {
+    const response = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: "AIzaSyApd3NMWiWyjhZL_yAd7NiIpcz7pPBf-Ec",
+        q: SearchTerm
+      }
+    });
+    console.log(response.data.items);
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <Grid justify="center" container spacing={10}>
+        <Grid item xs={12}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <SearchBar onFormSubmit={this.handleSubmit} />
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetail />
+            </Grid>
+            <Grid item xs={4}>
+              {/* Video List */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default App;
